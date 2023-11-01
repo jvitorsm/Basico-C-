43aa017a71b8acf1e53d82d279289e468ab195ee
João Vitor Siqueira de Moraes <joaovsm04@gmail.com>
2023-10-31 23:20:57 -0300

    commit teste

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+int main() {
+    
+    //int x; // declaration
+    //x = 5; // assing
+    
+    //int x = 5;
+    //int y = 6;
+    //int sum = x+y; 
+    //std :: cout << x << '\n' << y << '\n' << sum << '\n';
+     
+
+     //integer (Whole number)
+     int year = 2022;
+     int age = 22;
+     
+     //double (number including decimal)
+     double price = 29.99;
+     double temperature = 30.6;
+    
+    // char (single charecter)
+    char grade = 'A';
+    char initial = 'J';
+    char currency = '$';
+
+    // booleans (only two states [T/F])
+    bool student = true;
+    bool power = false; 
+
+    // string (objext that represent a sequence of text)
+    std::string name = "João Vitor"; 
+
+
+    std :: cout << price << '\n';
+    std :: cout << age << '\n';
+    std :: cout << temperature << '\n';
+    std :: cout << initial << '\n';
+    std :: cout << student << '\n';
+    std :: cout << "Hello "<<  name <<". Are you "<< age << " years old?" << '\n';
+
+
+
+    return 0;
+
+}
