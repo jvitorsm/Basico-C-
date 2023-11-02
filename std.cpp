366f07d68bf02d167ab709288fa047357ee5e417
jvitorsm <joaovsm04@gmail.com>
2023-11-02 20:13:34 -0300

    New file added

---
+++ std.cpp
@@ -0,0 +1,17 @@
+// The std is used when you declareing a string or calling a output.
+// then you can set std as standard
+// std have hundreds of entities. so you also can try std::cout our std::strind to cut some lines 
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age = 22;
+    string name = "João Vitor";
+
+    cout << "My name is " << name <<" and I have "<< age<< " years old";
+
+return 0;
+}
